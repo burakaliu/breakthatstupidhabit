3d2e0f7d9e477f93ef5b48dfe8208b0969cdcb31
Burak <burakaliunlu@gmail.com>
2025-02-13 12:35:32 -0500

    fixed bug: ryan's chart now appears in dashboard

--- menu.js
+++ menu.js
@@ -2,12 +2,13 @@ document.addEventListener('DOMContentLoaded', function() {
     const menuIcon = document.getElementById('menuIcon');
     const sideMenu = document.querySelector('.side-menu');
 
+    //listen fpr clicks on the menu to close or open
     menuIcon.addEventListener('click', function() {
         menuIcon.classList.toggle('active');
         sideMenu.classList.toggle('active');
     });
 
-    // Close menu when clicking outside
+    // close the menu when you click outside
     document.addEventListener('click', function(event) {
         if (!menuIcon.contains(event.target) && 
             !sideMenu.contains(event.target) && 
