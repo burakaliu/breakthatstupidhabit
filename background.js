0baaed01ade3f7813671f64af9716f8ea50ff187
adrito-art <70554283+adrito-art@users.noreply.github.com>
2025-02-25 08:24:50 -0500

    Add files via upload
    idk might work !

--- background.js
+++ background.js
@@ -1,11 +1,24 @@
 let currentTab = null;
 let lastUpdate = Date.now();
+let trackingEnabled = true;
+const distractionSites = [/* your distraction sites */];
+const productiveSites = [/* your productive sites */];
 
-chrome.tabs.onActivated.addListener(({tabId}) => {
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.storage.local.set({
+    timeLog: { productive: 0, distracting: 0, neutral: 0 },
+    websiteData: {},
+    dailyLog: {}
+  });
+});
+
+chrome.tabs.onActivated.addListener(({ tabId }) => {
   updateTime();
-  chrome.tabs.get(tabId, tab => {
-    if (tab?.url) currentTab = classifyTab(tab.url);
-    lastUpdate = Date.now();
+  chrome.tabs.get(tabId, (tab) => {
+    if (tab?.url) {
+      currentTab = classifyTab(tab.url);
+      lastUpdate = Date.now();
+    }
   });
 });
 
@@ -18,37 +31,39 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
 });
 
 function classifyTab(url) {
-  const {hostname} = new URL(url);
+  const { hostname } = new URL(url);
   let category = "neutral";
-  
   if (distractionSites.some(s => hostname.includes(s))) category = "distracting";
   else if (productiveSites.some(s => hostname.includes(s))) category = "productive";
-
-  chrome.storage.local.set({currentSite: hostname, status: category});
+  
+  chrome.storage.local.set({ 
+    currentSite: hostname, 
+    status: category,
+    lastUpdated: Date.now()
+  });
   return category;
 }
 
 function updateTime() {
-  if (!currentTab) return;
+  if (!currentTab || !trackingEnabled) return;
   
   const elapsed = (Date.now() - lastUpdate) / 1000;
-  chrome.storage.local.get(["timeLog"], ({timeLog = {}}) => {
-    timeLog[currentTab] = (timeLog[currentTab] || 0) + elapsed;
-    chrome.storage.local.set({timeLog});
-  });
+  const dateKey = new Date().toISOString().split('T')[0];
   
-  lastUpdate = Date.now();
+  chrome.storage.local.get(["timeLog", "dailyLog"], ({ timeLog, dailyLog }) => {
+    timeLog[currentTab] = (timeLog[currentTab] || 0) + elapsed;
+    dailyLog[dateKey] = (dailyLog[dateKey] || 0) + elapsed;
+    
+    chrome.storage.local.set({ timeLog, dailyLog });
+    lastUpdate = Date.now();
+  });
 }
 
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-  if (message.type === "timeUpdate") {
-    chrome.storage.local.get(['websiteData'], ({websiteData = {}}) => {
-      const {url, timeSpent} = message.data;
-      websiteData[url] = websiteData[url] || {totalTime: 0, visits: 0};
-      websiteData[url].totalTime += timeSpent;
-      websiteData[url].visits++;
-      websiteData[url].lastVisit = Date.now();
-      chrome.storage.local.set({websiteData});
+chrome.runtime.onMessage.addListener((message) => {
+  if (message.type === "toggleTracking") {
+    trackingEnabled = message.status;
+    chrome.action.setIcon({
+      path: trackingEnabled ? "icon.png" : "icon-disabled.png"
     });
   }
 });
